fe33cd39aae632f09aa369a7972aab525ad6e3ba
ipCoelho <ipcoelho.personal@gmail.com>
2021-10-04 16:55:20 -0300

    Dashboard Activity updated

---
+++ app/src/main/java/br/senai/sp/jandira/imcapp20/DashboardActivity.kt
@@ -0,0 +1,13 @@
+package br.senai.sp.jandira.imcapp20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+
+        supportActionBar!!.hide()
+    }
+}
--- app/src/main/java/br/senai/sp/jandira/imcapp20/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/imcapp20/MainActivity.kt
@@ -1,7 +1,9 @@
 package br.senai.sp.jandira.imcapp20
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +11,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportActionBar!!.hide()
+
+        val buttonEntrar = findViewById<Button>(R.id.btn_entrar)
+
+        buttonEntrar.setOnClickListener {
+            val openDashboardActivity = Intent(this, DashboardActivity::class.java)
+            startActivity(openDashboardActivity)
+        }
     }
 }
