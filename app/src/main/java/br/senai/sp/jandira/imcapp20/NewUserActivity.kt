bc4fff18bf7c6485364b4f41b27afc0f7d313c63
ipCoelho <ipcoelho.personal@gmail.com>
2021-10-18 16:55:28 -0300

    register activity done

---
+++ app/src/main/java/br/senai/sp/jandira/imcapp20/NewUserActivity.kt
@@ -0,0 +1,11 @@
+package br.senai.sp.jandira.imcapp20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_user)
+    }
+}
